93798c32e1d926f10341c6ee0a7010be76c1e95e
Jonas Ericsson <jonas@agilefrog.se>
2014-08-24 12:41:56 +0200

    Fixed setup data.
    There were some minor mistakes in setup.py which is fixed now.

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
         name="DotConfig",
-        version="0.1.0",
+        version="0.1.1",
         author="Jonas Ericsson",
         author_email="jonas@agilefrog.se",
         py_modules = ["dotconfig"],
-        url="http://www.foobar.com",
+        url="http://github.com/frawgie/dotconfig",
         license="LICENSE.txt",
-        description="",
+        description="A simpler way to read configuration files.",
         long_description=open("README.txt").read(),
         install_requires=[],
 )
